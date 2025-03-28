79fd55d3d5ef851ecfacc589866fb99b3c2a7b99
apoorva232 <56165757+apoorva232@users.noreply.github.com>
2025-03-28 18:26:27 +0530

    Create App.cs

---
+++ App.cs
@@ -0,0 +1,51 @@
+.App {
+  text-align: center;
+}
+
+input {
+  padding: 10px;
+  font-size: 16px;
+  width: 300px;
+  margin-right: 10px;
+}
+
+button {
+  padding: 10px;
+  font-size: 16px;
+  cursor: pointer;
+}
+
+.movie-list {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  margin-top: 20px;
+}
+
+.movie-item {
+  margin: 10px;
+  cursor: pointer;
+  text-align: center;
+}
+
+.movie-item img {
+  width: 150px;
+  height: 200px;
+  object-fit: cover;
+}
+
+.movie-detail {
+  text-align: left;
+  max-width: 600px;
+  margin: auto;
+}
+
+.movie-detail img {
+  width: 200px;
+  height: 300px;
+  object-fit: cover;
+}
+
+.movie-detail button {
+  margin-top: 10px;
+}
